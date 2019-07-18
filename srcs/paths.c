1020d4113516cfee75f3c30bce5dd22e85a886f0
epham <epham@student.42.fr>
2019-07-18 18:17:01 +0200

    not :fire: yet

--- srcs/paths.c
+++ srcs/paths.c
@@ -6,13 +6,113 @@
 /*   By: anradixt <anradix@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 11:57:31 by epham             #+#    #+#             */
-/*   Updated: 2019/07/18 15:57:45 by anradixt         ###   ########.fr       */
+/*   Updated: 2019/07/18 18:16:31 by anradixt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
 
-t_path      next_room(t_room current)
+/*
+CAS ERREUR
+
+- Pas de link sortant de start
+*/
+
+
+/*
+***     CREATING PATH WITH START ROOM
+*/
+
+t_path      *start_path(t_room *start)
+{
+    t_path  path;
+
+    if (start->linked_rooms == NULL)
+        return (NULL);
+    if (!(path = malloc(sizeof(t_path))))
+        return (NULL);
+    path->room = start;
+    path->link = NULL;
+    path->prev = NULL;
+    path->next = NULL;
+    return (path);
+}
+
+/*
+***     GET NEXT ROOM
+*/
+
+t_room      get_next_room(t_room *source)
 {
     
+}
+
+/*
+***     CHECKS THAT ROOM IS NOT ALREADY IN PATH
+*/
+
+t_path         check_pathroom(t_room *room, t_path *path)
+{
+    t_room *head;
+
+    head = path;
+    while (path->next == NULL)
+    {
+        if (path->room == room)
+        {
+            remove_path(head);
+            return (NULL);
+        }
+        path = path->next;
+    }
+    return (head);
+}
+
+/*
+***     CREATE PATHNODE WITH NEW ROOM
+*/
+
+t_path      *create_pathroom(t_room *room, t_link *link)
+{
+    t_path  pathroom;
+
+    if (!(pathroom = malloc(sizeof(t_path))))
+        return (NULL);
+    pathroom->room = room;
+    pathroom->link = link;
+    pathroom->next = NULL;
+    return (pathroom);
+}
+
+/*
+***     ADD PATH NODE TO PATH
+*/
+
+t_path      *append_node(t_path *path, t_path *newroom)
+{
+    t_path  *head;
+    t_path  *prev;
+
+    head = path;
+    while (path->next != NULL)
+    {
+        if (path->next->next == NULL)
+            prev = path;
+        path = path->next;
+    }
+    path = newroom;
+    path->prev = prev;
+    return (head);
+}
+
+/*
+***     DELETING INVALID PATH
+*/
+
+void        remove_path(t_path *head)
+{
+    while (head->next != NULL)
+    {
+        
+    }
 }
